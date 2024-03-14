0b8446642dbae25babdbdf9e9c93357b603ae38d
Nicholas Githinji <113605917+nickdelicto@users.noreply.github.com>
2024-03-14 23:19:48 +0300

    Add paths to template files within 'content' array
    So that Tailwind can scan the paths for class names and generate
    corresponding styles.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js, jsx, ts, tsx}", // This line includes all JavaScript and TypeScript files in your src/ directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

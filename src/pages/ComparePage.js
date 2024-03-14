bbdddac3efb5291c4a5c811baa19ef4309ae27e2
Nicholas Githinji <113605917+nickdelicto@users.noreply.github.com>
2024-03-14 16:29:31 +0300

    Initial commit

---
+++ src/pages/ComparePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HomePage() {
+    return <div>Compare Health Insurance</div>;
+}
+
+export default HomePage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HomePage() {
+    return <div>Welcome to PolicyMatch Health Insurance Comparison Platform!</div>;
+}
+
+export default HomePage;

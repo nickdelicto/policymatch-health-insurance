b24dd5f588b2182af14f189ac52704eb0e1acd06
Nicholas Githinji <113605917+nickdelicto@users.noreply.github.com>
2024-03-18 05:32:08 +0300

    Include a page to host User Selection Form

---
+++ src/pages/GetStartedPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserSelectionForm from './UserSelectionForm'; // This being my form component
+
+const GetStartedPage = () => {
+    return (
+        <div>
+            <h2>Get Started with Your Insurance Plan</h2>
+            <UserSelectionForm/>
+        </div>
+    );
+};
+
+export default GetStartedPage;
